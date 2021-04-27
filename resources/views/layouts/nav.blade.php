3ceff0cd514d06b01cf229e33ae260af134197d3
zwaaryo <82909285+zwaaryo@users.noreply.github.com>
2021-04-27 03:10:48 +0300

    Update nav.blade.php

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -7,6 +7,7 @@
         <title>{{ config('app.name', 'Zwaar') }}</title>
     <link href="{{ asset('style.css') }}" rel="stylesheet">
     <link href="{{ asset('home.css') }}" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/71834eba85.js" crossorigin="anonymous"></script>
 
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
