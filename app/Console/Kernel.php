0c3214f7289d43527eb4bda4957399062b4a7d7e
zwaaryo <82909285+zwaaryo@users.noreply.github.com>
2021-04-27 06:27:29 +0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+ 
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +14,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+  \App\console\Commands\Links::class,
+ 
     ];
 
     /**
@@ -25,6 +27,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('links')->everyMinute();
+    
     }
 
     /**
