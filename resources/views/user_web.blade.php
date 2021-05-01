1c24c7159ae01d29c9e40646362ab8157e3a9323
zwaaryo <82909285+zwaaryo@users.noreply.github.com>
2021-05-02 01:52:19 +0300

    Update user_web.blade.php

--- resources/views/user_web.blade.php
+++ resources/views/user_web.blade.php
@@ -5,8 +5,8 @@
  <!-- صفحه عرض مقاطع الفيديو للزوار -->
     @foreach($links as $link)     
       @if($link->website_number === 1)
-   
-  <iframe class='cards' src="{{$link['link']}}?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+   //cards
+  <iframe class='' src="{{$link['link']}}?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
  
    
  @endif
